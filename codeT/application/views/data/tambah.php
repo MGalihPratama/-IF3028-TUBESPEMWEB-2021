cf5453291092c7e272ca725fb90aa8ed50c8bb15
118140118 - Putriayuni <56281908+Putriayuni118140118@users.noreply.github.com>
2020-12-19 20:28:36 +0700

    Update Tambah Data

--- codeT/application/views/data/tambah.php
+++ codeT/application/views/data/tambah.php
@@ -1,8 +1,9 @@
 <fieldset>
 	<br><br>
 	<div class="wrapper">
-		<h1>SIMPEL LAPOR!</h1>
+		<h1> LAPOR.ID </h1>
 	</div>
+
 	<?= validation_errors(); ?>
 	<div class="tambah">
 		<form name="formTambah" action="" onsubmit="return validasiKeluhan()" method="post" enctype="multipart/form-data">
@@ -11,13 +12,13 @@
 			<input type="text" name="username" id="username" readonly value="<?= $user['username'] ?>">
 			<br><br>
 			<label for="lapor">Buat Laporan/Komentar
-				<hr />
 				<br>
 			</label>
-			<textarea name="lapor" id="lapor" placeholder="Inputkan Komentar anda dengan minimal 20 kata" rows="4" cols="50"></textarea>
+			<br>
+			<textarea name="lapor" id="lapor" placeholder="Masukan komentar anda. Minimal 20 Kata " rows="4" cols="50"></textarea>
 			<br><br>
 			<select id="aspek" name="aspek">
-				<option value="" hidden>Pilih Aspek/komentar</option>
+				<option value="" hidden>Aspek</option>
 				<option>Dosen</option>
 				<option>Staff</option>
 				<option>Mahasiswa</option>
@@ -28,9 +29,8 @@
 			<input type="file" name="gambar" id="gambar">
 			<br><br>
 			<div style="text-align: right;">
-				<button type="submit" id="submit" onclick="return checkWordCount()" name="submit">Buat Lapor</buttons>
+				<button type="submit" id="submit" onclick="return checkWordCount()" name="submit"> Buat Lapor</buttons>
 			</div>
 		</form>
 	</div>
-
 </fieldset>
