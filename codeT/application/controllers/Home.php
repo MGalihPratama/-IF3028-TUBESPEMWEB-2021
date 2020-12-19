000f696302a7e31cbaa11b3d5610e363921b2e2d
manarulh <65214965+manarulh@users.noreply.github.com>
2020-12-19 18:48:21 +0700

    Controller untuk views home

---
+++ codeT/application/controllers/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+class Home extends CI_Controller
+{
+	public function index()
+	{
+		$this->load->model('Data_model');
+		$data['judul'] = 'Home';
+		$data['link'] = 'home';
+		$data['lapor'] = $this->Data_model->getDataLapor();
+		if ($this->input->post('keyword')) {
+			$data['lapor'] = $this->Data_model->cariDataKeluhan();
+		}
+		$this->load->view('templates/header', $data);
+		$this->load->view('home/index', $data);
+		$this->load->view('templates/footer');
+	}
+}
