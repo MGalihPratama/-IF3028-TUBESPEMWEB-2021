8986481385f9218c8f7d53aa90b971c37d208648
118140118 - Putriayuni <56281908+Putriayuni118140118@users.noreply.github.com>
2020-12-19 20:21:18 +0700

    Update detail.php

--- codeT/application/views/data/detail.php
+++ codeT/application/views/data/detail.php
@@ -1,12 +1,12 @@
 <fieldset>
     <br><br>
     <div class="wrapper">
-        <h1>SIMPEL LAPOR</h1>
+        <h1> LAPOR.ID </h1>
     </div>
     <h3>Detail laporan/Komentar oleh <?= $keluhan['username'] ?></h3>
     <div class="pusing">
         <p><?= $keluhan['lapor']; ?></p>
-        <p><b>Lampiran :</b></p>
+        <p><b> Lampiran :</b></p>
         <div id="container_detail">
             <div id="si_foto">
                 <img src="<?php echo base_url(); ?>asset/gambar/<?php echo $keluhan['gambar']; ?>" widht="400" height="550">
@@ -14,12 +14,11 @@
             <br>
             <table id="footer_detail">
                 <tr>
-                    <td id="si_waktu"> Waktu: <?= $keluhan['waktu']; ?>_
-                        _Aspek: <?= $keluhan['aspek']; ?> </td>
+                    <td id="si_waktu"> Waktu : <?= $keluhan['waktu']; ?> | Aspek : <?= $keluhan['aspek']; ?> </td>
                     <td id="pening">
                         <a href="<?= base_url(); ?>data/ubah/<?= $keluhan['id']; ?>">Ubah | </a>
-                        <a href="<?= base_url(); ?>data/hapus/<?= $keluhan['id']; ?>" onclick="return confirm('Ingin Menghapus')">Hapus</a>
-                        <a href="<?= base_url('user/index'); ?>">| Kembali</a>
+                        <a href="<?= base_url(); ?>data/hapus/<?= $keluhan['id']; ?>" onclick="return confirm('Ingin Menghapus ?')">Hapus</a>
+                        <a href="<?= base_url('user/index'); ?>">| < Kembali </a>
                     </td>
                 </tr>
             </table>
