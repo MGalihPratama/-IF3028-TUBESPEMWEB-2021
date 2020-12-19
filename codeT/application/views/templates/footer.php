ee12a2de2fe0eb5d63246a68c8c6424ca9d175b8
118140118 - Putriayuni <56281908+Putriayuni118140118@users.noreply.github.com>
2020-12-19 19:43:36 +0700

    Footer

--- codeT/application/views/templates/footer.php
+++ codeT/application/views/templates/footer.php
@@ -1,10 +1,9 @@
 <div class="footer">
   <br>
-  <h3>&copy; Copyright by Pejuang Tubes </h3S>
-    <h5> M. GALIH PRATAMA</h5S>
-      <h5>Putri Ayuni</h5S>
-        <h5>Manarul Hidayat</h5S>
+  <h3>&copy;  Pejuang_Tubes </h3S>
+    <h5> M Galih Pratama | NIM. 118140102 </h5S>
+      <h5> Putri Ayuni S | NIM. 118140118 </h5S>
+        <h5> Manarul Hidayat | NIM. 118140132 </h5S>
           <hr />
           </body>
-
           </html>
