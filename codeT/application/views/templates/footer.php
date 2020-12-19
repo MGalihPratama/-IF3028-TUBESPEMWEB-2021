d5942c70bc36410c75ddd22a9ebe364850a56a6b
M. Galih Pratama <mgalih.118140102@student.itera.ac.id>
2020-12-19 18:17:51 +0700

    Fix footer aplikasi

---
+++ codeT/application/views/templates/footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+  <br>
+  <h3>&copy; Copyright by Pejuang Tubes </h3S>
+    <h5> M. GALIH PRATAMA</h5S>
+      <h5>Putri Ayuni</h5S>
+        <h5>Manarul Hidayat</h5S>
+          <hr />
+          </body>
+
+          </html>
